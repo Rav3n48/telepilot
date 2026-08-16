09a4b54a53c4454e1d4381f7e8c6109a4e1d64e7
Raven <r4v3n48@gmail.com>
2026-08-16 12:57:14 +0330

    added proxy and application builder telegram apis needs proxy in some countries. so I added PROXY_URL to env for easier proxy settings

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import asyncio
 
-from config import BOT_TOKEN
+from telegram.ext import Application
+
+from config import BOT_TOKEN, PROXY_URL
 from db.session import init_db
+from handlers import register_handlers
 
 
 async def main():
-    print(f"Starting Telepilot with token: {BOT_TOKEN[:8]}...")
+    print(f"Starting Telepilot...")
     await init_db()
+
+    application_builder = Application.builder().token(BOT_TOKEN)
+    if PROXY_URL:
+        application_builder.proxy(PROXY_URL).get_updates_proxy(PROXY_URL)
+    application = application_builder.build()
+    register_handlers(application)
+    await application.initialize()
+    await application.start()
+    await application.updater.start_polling()
+
+    try:
+        print("bot is running...")
+        await asyncio.Event().wait()
+    finally:
+        await application.updater.stop()
+        await application.stop()
+        await application.shutdown()
 
 
 if __name__ == "__main__":
